2d6a1a88bfe8a8a2604fa7381e46145f28c3118c
John Ruffer <jqruffer@vt.edu>
2012-11-19 15:42:43 -0500

    added stuff to all tests test suite. Kind of weird, because I'm bad at this, but tests are order dependent, and the text files must be cleared first.

--- src/gissystem/tests/AllTests.java
+++ src/gissystem/tests/AllTests.java
@@ -1,20 +1,15 @@
 package gissystem.tests;
 
-import gissystem.tests.controllertests.DatabaseControllerTests;
-import gissystem.tests.controllertests.QuadTreeControllerTests;
-import gissystem.tests.datastructuretests.BufferPoolTests;
-import gissystem.tests.datastructuretests.HashTableTests;
-import gissystem.tests.datastructuretests.PrQuadtreeTests;
-import gissystem.tests.factorytests.GeographicCoordinateFactoryTests;
-import gissystem.tests.factorytests.GeographicFeatureFactoryTests;
-
+import gissystem.tests.commandtests.CommandTests;
+import gissystem.tests.controllertests.ControllerTests;
+import gissystem.tests.datastructuretests.DataStructuresTests;
+import gissystem.tests.factorytests.FactoriesTests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ DatabaseControllerTests.class, QuadTreeControllerTests.class, BufferPoolTests.class, HashTableTests.class, PrQuadtreeTests.class,
-	GeographicCoordinateFactoryTests.class, GeographicFeatureFactoryTests.class })
+@SuiteClasses({ ControllerTests.class, DataStructuresTests.class, FactoriesTests.class, CommandTests.class })
 public class AllTests {
 
 }
