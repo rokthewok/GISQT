db38c6f7e5e7928d0b643270c051448606d0a185
John Ruffer <jqruffer@vt.edu>
2012-11-19 20:25:41 -0500

    Finished implementing setup and work methods in GisSystemController. Set up the main method in GisSystem.

--- src/gissystem/GisSystem.java
+++ src/gissystem/GisSystem.java
@@ -1,29 +1,13 @@
 package gissystem;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.RandomAccessFile;
-
-import gissystem.controllers.DataAccessController;
 import gissystem.controllers.GisSystemController;
-import gissystem.helpers.io.FileLogger;
 
 public class GisSystem {
 	
 	public static void main( String [] args ) {
-		RandomAccessFile commandFile = null;
-		RandomAccessFile logFile = null;
-		try {
-			commandFile = new RandomAccessFile( "Script01.txt", "r" );
-			logFile = new RandomAccessFile( "log.txt", "rw" );
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+		GisSystemController controller = new GisSystemController();
 		
-		File databaseFile = new File( "database.txt" );
-		if( commandFile != null && logFile != null ) {
-			GisSystemController controller = new GisSystemController( new DataAccessController( databaseFile, commandFile, new FileLogger( logFile ) ) );
-			controller.doWork();
-		}
+		controller.doSetup( args );
+		controller.doWork();
 	}
 }
--- src/gissystem/controllers/GisSystemController.java
+++ src/gissystem/controllers/GisSystemController.java
@@ -1,6 +1,11 @@
 package gissystem.controllers;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+
 import gissystem.helpers.RawCommandParser;
+import gissystem.helpers.io.FileLogger;
 import gissystem.interfaces.ICommand;
 import gissystem.interfaces.IDataAccessController;
 
@@ -11,11 +16,15 @@ public class GisSystemController {
 	
 	private IDataAccessController dataAccessController;
 	
-	public GisSystemController( IDataAccessController dataAccessController ) {
-		this.dataAccessController = dataAccessController;
+	public GisSystemController() {
+		this.dataAccessController = null;
 	}
 	
 	public void doWork() {
+		if( this.dataAccessController == null ) {
+			return;
+		}
+		
 		RawCommandParser commandParser = new RawCommandParser();
 		
 		int count = 1;
@@ -43,13 +52,35 @@ public class GisSystemController {
 	}
 	
 	public void doSetup( String [] args ) {
+		parseArguments( args );
 		
+		RandomAccessFile commandFile = null;
+		RandomAccessFile logFile = null;
+		try {
+			commandFile = new RandomAccessFile( this.commandFilename, "r" );
+			logFile = new RandomAccessFile( this.logFilename, "rw" );
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		File databaseFile = new File( this.databaseFilename );
+
+		if( commandFile == null || logFile == null ) {
+			System.out.println( "Something went wrong with the command file or log file... please try again. Exiting now." );
+			System.exit( -1 );
+		}
+		
+		this.dataAccessController = new DataAccessController( databaseFile, commandFile, new FileLogger( logFile ) );
 	}
 	
 	private void parseArguments( String [] args ) {
 		if( args.length < 3 ) {
-			System.out.println( "Sorry, please provide the necessary number of arguments." );
+			System.out.println( "Sorry, please provide the necessary number of arguments. Exiting now." );
 			System.exit( -1 );
 		}
+		
+		this.databaseFilename = args[0];
+		this.commandFilename = args[1];
+		this.logFilename = args[2];
 	}
 }
